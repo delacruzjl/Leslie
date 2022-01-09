260db9486409d2724b24552e37f1c81a0b936342
Jose DelaCruz <jose@delacruzhome.com>
2022-01-09 07:38:06 -0500

    Initial commit

---
+++ apps/menu-e2e/src/integration/menu/menu.spec.ts
@@ -0,0 +1,7 @@
+describe('menu: Menu component', () => {
+  beforeEach(() => cy.visit('/iframe.html?id=menu--primary'));
+    
+    it('should render the component', () => {
+      cy.get('h1').should('contain', 'Welcome to Menu!');
+    });
+});
---
+++ libs/menu/src/lib/menu.tsx
@@ -0,0 +1,14 @@
+import './menu.module.scss';
+
+/* eslint-disable-next-line */
+export interface MenuProps {}
+
+export function Menu(props: MenuProps) {
+  return (
+    <div>
+      <h1>Welcome to Menu!</h1>
+    </div>
+  );
+}
+
+export default Menu;
